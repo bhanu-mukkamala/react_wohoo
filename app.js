95170eb45ab5ad70acb6c2165d337c2c6a3ad145
bhanu mukkamala <bmukkamala68@gmail.com>
2024-09-28 03:00:38 +0530

    Third episode

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},
-  [React.createElement("div",{id:"child1"},
-            [React.createElement("h1",{id:"nestedh1"},"This is a React Wohoo!"),React.createElement("h2",{id:"nestedh2"},"By B_Mukkamala")]),
-            React.createElement("div",{id:"child2"},
-            [React.createElement("h1",{id:"nestedh1"},"I'm an h1 tag(nested btw)"),React.createElement("h2",{id:"nestedh2"},"I'm an h2 tag")])]);
-const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// React element using core react, to creat an html element
+const heading = React.createElement("h1",{id : "heading"},"React Wohoo!");
+
+// Creating a react element using JSX syntax  
+const JsxHeading = (
+<h1 id="heading">
+  React Wohoo using JSX!
+  </h1>)
+//React functional component
+  const HeadingComponent = () => {
+    return<div id="container"><h1 className="heading">Namaste React component using js arrow function</h1></div>
+  }
+  //React functional component written using shorthand notation
+  const HeadingComponent2 =() =><h1 className="heading">Namaste React Component using arrow shorthand notation</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);

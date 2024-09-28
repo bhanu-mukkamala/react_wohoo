31d92b9eec530279678a458f7849edd15c9185a2
bhanu mukkamala <bmukkamala68@gmail.com>
2024-09-28 05:33:12 +0530

    React components is the main thing that i ahve learn today

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React element using core react, to creat an html element
-const heading = React.createElement("h1",{id : "heading"},"React Wohoo!");
+const Title = () => (
+  <h1 className="head">Namaste React!</h1>
+)
+const number = 10000;
 
-// Creating a react element using JSX syntax  
-const JsxHeading = (
-<h1 id="heading">
-  React Wohoo using JSX!
-  </h1>)
-//React functional component
-  const HeadingComponent = () => {
-    return<div id="container"><h1 className="heading">Namaste React component using js arrow function</h1></div>
-  }
-  //React functional component written using shorthand notation
-  const HeadingComponent2 =() =><h1 className="heading">Namaste React Component using arrow shorthand notation</h1>;
-
+//Component composition
+  const HeadingComponent = () => (
+    <div id="container">
+      <h1>{number}</h1>
+    <h1 className="heading">Namaste React component using js arrow function</h1>
+        </div>
+  );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent/>);
